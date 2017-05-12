b58872d67589eeb88af6e89444b6866144ff6159
Onadius <orezigokusyounenn@gmail.com>
2017-05-12 15:13:11 +0900

    構造体配列削除->自己参照構造体

--- C言語/kadai/kadai9.c
+++ C言語/kadai/kadai9.c
@@ -8,7 +8,6 @@
 #include <string.h>
 
 #define MOZINUM 256
-#define FILEROW 275
 
 /* 構造体宣言 */
 struct services {
@@ -20,25 +19,27 @@ struct services {
 
 
 /* 関数プロトタイプ宣言 */
-void extructStr(char *str, int i, struct services *miku) ;
-void setNext(struct services *miku) ;
-void outPut(struct services *miku) ;
+void extructStr(char *str, struct services *newdata) ;
+void output(struct services *wp, struct services *start) ;
 
 
 /* main関数 */
 int main(void){
 
-  struct services sagiri[FILEROW] ;
-  struct services *miku ; /* 作業用 */
-
   char fname[] = "services.txt" ;
   FILE *fp ;
   char str[MOZINUM] ;
-  int  i = 0 ;
-  char *cpy ;
 
-  /*ポインタmikuに構造体配列の先頭アドレスsagiriを設定(初期化)*/
-  miku = sagiri ;
+
+  /* -- 自己参照構造体定義 -- */
+  struct services dmy ;
+  struct services *start = &dmy ; //最初の構造体
+  struct services *newdata ; // 最新データ格納用
+  struct services *wp ; // 現作業用
+
+  start->next = NULL;
+  wp = start; //初期化している (1)
+
 
   /*ファイルオープン*/
   if((fp = fopen(fname, "r")) == NULL){
@@ -53,74 +54,76 @@ int main(void){
       continue ;
     }
 
-    /* 文字列抽出関数呼び出し */
-    cpy = str ;
-    extructStr(cpy, i, miku) ;
 
-    i++ ;
+    /* 以下、自己参照構造体への格納処理 */
+    newdata = (struct services *)malloc(sizeof(struct services)); /* 領域確保 */
+    if (newdata == NULL){
+      printf("sonnna namae no hito siranai !\n") ; /* 領域確保不可 */
+      break ;
+    }
+
+    /* 文字列抽出関数呼び出し */
+    extructStr(str, newdata) ;
+
+    if( wp->next == NULL ) {
+
+      /* newdata->next をNULLにする。NULLは最後のデータの印 */
+      newdata->next = NULL ;
+
+      /* wpはnewdataよりひとつ前のpersonal構造体変数。
+      ひとつ前のnextにnewdataを設定し、チェーンを作る */
+      wp->next = newdata ;
+
+      /* wpを最新のpersonal構造体を指し示すようにする (1) */
+      wp = newdata ;
+    }
   }
   fclose(fp) ;
 
-  /* 自己参照構造体定義関数呼び出し */
-  setNext(miku) ;
+  /* 出力 */
+  output(wp, start) ;
 
-  /* 出力関数呼び出し */
-  outPut(miku) ;
-
+  free(newdata) ;
   return 0 ;
 }
 
 
-
 /* 文字列抽出及び構造体配列への格納関数 */
-void extructStr(char *str, int i, struct services *miku) {
+void extructStr(char *str, struct services *newdata) {
 
   char *s1, *s2, *s3 ;
 
   s1 = strtok(str, " /") ;
-  strcpy((miku + i)->service_name, s1) ;
+  strcpy(newdata->service_name, s1) ;
 
-  /* (miku + i)->port_numberには、i行目の数字から/までを抽出 */
   str = strtok(NULL, " /") ;
   s2 = str ;
-  strcpy((miku + i)->port_number, s2);
+  strcpy(newdata->port_number, s2);
 
-  /* (miku + i)->protocolには、i行目の/から空白までを抽出 */
   str = strtok(NULL, " /") ;
   s3 = str ;
-  strcpy((miku + i)->protocol, s3);
-}
+  strcpy(newdata->protocol, s3);
 
-
-/* 自己参照構造体定義関数  */
-void setNext(struct services *miku) {
-
-  struct services dmy ;
-  struct services *start = &dmy ;
-  int i ;
-
-  start->next = miku ; /* 初期化 */
-  miku->next = (miku + 1) ; /*初期化*/
-
-  for( i = 1 ; i <= FILEROW ; i ++) {
-
-    if( i == FILEROW ) {
-      (miku + i)->next = NULL ;
-    }
-
-    (miku + i)->next = (miku + (i + 1)) ;
-  }
 }
 
 
 /* 出力関数 */
-void outPut(struct services *miku) {
-  int j ;
-  /* 出力 */
-  for( j = 0 ; j < FILEROW ; j++ ) {
-    printf("<service name>:%s  <port number>:%s  <protocol>:%s\n",
-      (miku + j)->service_name,
-      (miku + j)->port_number,
-      (miku + j)->protocol) ;
+void output(struct services *wp, struct services *start) {
+
+  for( wp = start->next ; wp != NULL ; wp = wp->next ){
+    printf("[serviceName] : %s  [portNumber] : %s  [Protocol] : %s\n",
+    wp->service_name, wp->port_number, wp->protocol);
   }
 }
+
+/* -- 結果
+
+[serviceName] : echo  [portNumber] : 7  [Protocol] : tcp
+[serviceName] : echo  [portNumber] : 7  [Protocol] : udp
+[serviceName] : discard  [portNumber] : 9  [Protocol] : tcp
+[serviceName] : discard  [portNumber] : 9  [Protocol] : udp
+...
+[serviceName] : directplaysrvr  [portNumber] : 47624  [Protocol] : tcp
+[serviceName] : directplaysrvr  [portNumber] : 47624  [Protocol] : udp
+
+*/
