d5c809878630c45df0609a1c2a0fed5dd776c386
Onadius <orezigokusyounenn@gmail.com>
2017-05-25 10:19:40 +0900

    バブルソート完成。コーディング整形完了

--- C言語/kadai/kadai11.c
+++ C言語/kadai/kadai11.c
@@ -21,10 +21,10 @@ struct services {
 
 /* 関数プロトタイプ宣言 */
 void extructStr(char *str, struct services *newdata) ;
-void output(struct services *wp, struct services *start, struct services dmy) ;
 void Replace(struct services *honnmono, struct services *nisemono) ;
 void Unlink(struct services *element) ;
 void Link(struct services *NextElem, struct services *LinkElem) ;
+void output(struct services *wp, struct services *start) ;
 
 
 /* main関数 */
@@ -37,15 +37,15 @@ int main(void){
 
   /* -- 自己参照構造体定義 -- */
   struct services dmy ;
-  struct services *start = &dmy ; //先頭の構造体
-  struct services *newdata ; // 最新データ格納用
-  struct services *wp ; // 現在位置用
+  struct services *start = &dmy ; /* 先頭の構造体 */
+  struct services *newdata ; /* 最新データ格納用 */
+  struct services *wp ; /* 現在位置用 */
   struct services *nisemono ;
   struct services *honnmono ;
 
   start->next = NULL ;
   start->before = NULL ;
-  wp = start ; //初期化している (1)
+  wp = start ; /* 初期化している (1) */
 
 
   /* -- ファイルオープン -- */
@@ -93,11 +93,8 @@ int main(void){
   start->before = wp ;
   wp->next = start ;
 
-  /* ファイル出力 before順に */
-  output(wp, start, dmy) ;
 
-
-  /*------------------ sort関数作成途中 ------------------------*/
+  /*------------------ バブルソート処理 ------------------------*/
   honnmono = wp ;
   nisemono = wp ;
 
@@ -105,29 +102,18 @@ int main(void){
     for(  nisemono = honnmono->next ; nisemono != start ; nisemono = nisemono->next ) {
 
       ret = strcmp(nisemono->service_name, honnmono->service_name) ;
-      //printf("%d\n", ret) ;
 
       if( ret == -1 ) {
+
+        /* バブルソート開始関数 */
         Replace(honnmono, nisemono) ;
         honnmono = nisemono ;
       }
     }
   }
 
-
-  printf("We'll rock you!!\n");
-  for( honnmono = start ; honnmono->next != start ; honnmono = honnmono->next ) {
-
-    printf("[serviceName] : %s  [portNumber] : %s  [Protocol] : %s\n",
-    honnmono->service_name, honnmono->port_number, honnmono->protocol);
-  }
-
-  /*------------------------------------------*/
-/*
-xe: cannot open output file
-C:\Users\ADMINI~1\AppData\Local\Temp\kadai11.exe: Permission denied
-collect2.exe: error: ld returned 1 exit status
-*/
+  /* --- 出力関数 ---*/
+  output(honnmono, start) ;
 
   free(newdata) ;
   fclose(fp) ;
@@ -135,6 +121,7 @@ collect2.exe: error: ld returned 1 exit status
 }
 
 /*  ------------------------------------------------------------------- */
+
 
 /* 文字列抽出及び構造体配列への格納関数 */
 void extructStr(char *str, struct services *newdata) {
@@ -148,15 +135,14 @@ void extructStr(char *str, struct services *newdata) {
   s2 = str ;
   strcpy(newdata->port_number, s2);
 
-  str = strtok(NULL, " /") ;
+  str = strtok(NULL, " /\n") ;
   s3 = str ;
   strcpy(newdata->protocol, s3);
 
 }
 
-
 /* ファイル出力関数 */
-void output(struct services *wp, struct services *start, struct services dmy) {
+void output(struct services *wp, struct services *start) {
 
   /*出力ファイル用変数*/
   char fname[] = "mikumiku.txt" ;
@@ -189,7 +175,7 @@ void Replace(struct services *honnmono, struct services *nisemono) {
   Unlink(honnmono) ;
   Unlink(nisemono) ;
 
-  Link(Back->next, nisemono) ; //Back->nextには何もないよ
+  Link(Back->next, nisemono) ;
   Link(Next, honnmono) ;
 
 }
@@ -227,11 +213,14 @@ void Link(struct services *NextElem, struct services *LinkElem) {
 
 /* -- 結果
 
-[serviceName] :   [portNumber] :   [Protocol] :
-[serviceName] : echo  [portNumber] : 7  [Protocol] : tcp
-[serviceName] : echo  [portNumber] : 7  [Protocol] : udp
-...
-[serviceName] : imip-channels  [portNumber] : 11320  [Protocol] : udp
-[serviceName] : directplaysrvr  [portNumber] : 47624  [Protocol] : tcp
+[serviceName] :   [portNumber] : �Kv  [Protocol] :
+[serviceName] : activesync  [portNumber] : 1034  [Protocol] : tcp
+[serviceName] : afpovertcp  [portNumber] : 548  [Protocol] : udp
+[serviceName] : afpovertcp  [portNumber] : 548  [Protocol] : tcp
+[serviceName] : auth  [portNumber] : 113  [Protocol] : tcp
+[serviceName] : bgp  [portNumber] : 179  [Protocol] : tcp
+[serviceName] : biff  [portNumber] : 512  [Protocol] : udp
+[serviceName] : bootpc  [portNumber] : 68  [Protocol] : udp
+・・・
 
 */
